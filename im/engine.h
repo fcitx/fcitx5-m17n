331b25a6480a5daf591cf862e1bd90467f638216
Weng Xuetian <wengxt@gmail.com>
2019-11-11 00:01:11 -0800

    replace clang-format.sh with git hook.

--- im/engine.h
+++ im/engine.h
@@ -36,73 +36,72 @@ FCITX_CONFIGURATION(M17NConfig, Option<bool> enableDeprecated{
 
 class M17NData : public InputMethodEntryUserData {
 public:
-    M17NData(MSymbol language, MSymbol name)
-        : language_(language), name_(name) {}
+  M17NData(MSymbol language, MSymbol name) : language_(language), name_(name) {}
 
-    MSymbol language() const { return language_; }
-    MSymbol name() const { return name_; }
+  MSymbol language() const { return language_; }
+  MSymbol name() const { return name_; }
 
 private:
-    MSymbol language_;
-    MSymbol name_;
+  MSymbol language_;
+  MSymbol name_;
 };
 
 class M17NEngine;
 
 class M17NState : public InputContextProperty {
 public:
-    M17NState(M17NEngine *engine, InputContext *ic)
-        : engine_(engine), ic_(ic), mim_(nullptr, &minput_close_im),
-          mic_(nullptr, &minput_destroy_ic) {}
+  M17NState(M17NEngine *engine, InputContext *ic)
+      : engine_(engine), ic_(ic), mim_(nullptr, &minput_close_im),
+        mic_(nullptr, &minput_destroy_ic) {}
 
-    void keyEvent(const InputMethodEntry &entry, KeyEvent &keyEvent);
-    void command(MInputContext *context, MSymbol command);
-    void updateUI();
-    void select(int index);
-    void reset();
-    bool keyEvent(const Key &key);
+  void keyEvent(const InputMethodEntry &entry, KeyEvent &keyEvent);
+  void command(MInputContext *context, MSymbol command);
+  void updateUI();
+  void select(int index);
+  void reset();
+  bool keyEvent(const Key &key);
 
-    static void callback(MInputContext *context, MSymbol command);
+  static void callback(MInputContext *context, MSymbol command);
 
-    M17NEngine *engine_;
-    InputContext *ic_;
-    std::unique_ptr<MInputMethod, decltype(&minput_close_im)> mim_;
-    std::unique_ptr<MInputContext, decltype(&minput_destroy_ic)> mic_;
+  M17NEngine *engine_;
+  InputContext *ic_;
+  std::unique_ptr<MInputMethod, decltype(&minput_close_im)> mim_;
+  std::unique_ptr<MInputContext, decltype(&minput_destroy_ic)> mic_;
 };
 
 class M17NEngine : public InputMethodEngine {
 public:
-    M17NEngine(Instance *instance);
+  M17NEngine(Instance *instance);
 
-    void activate(const fcitx::InputMethodEntry &,
-                  fcitx::InputContextEvent &) override;
-    void keyEvent(const fcitx::InputMethodEntry &entry,
-                  fcitx::KeyEvent &keyEvent) override;
-    void reloadConfig() override;
-    void reset(const fcitx::InputMethodEntry &,
-               fcitx::InputContextEvent &) override;
-    auto factory() { return &factory_; }
+  void activate(const fcitx::InputMethodEntry &,
+                fcitx::InputContextEvent &) override;
+  void keyEvent(const fcitx::InputMethodEntry &entry,
+                fcitx::KeyEvent &keyEvent) override;
+  void reloadConfig() override;
+  void reset(const fcitx::InputMethodEntry &,
+             fcitx::InputContextEvent &) override;
+  auto factory() { return &factory_; }
 
-    const Configuration *getConfig() const override { return &config_; }
-    void setConfig(const RawConfig &config) override {
-        config_.load(config, true);
-        safeSaveAsIni(config_, "conf/m17n.conf");
-    }
+  const Configuration *getConfig() const override { return &config_; }
+  void setConfig(const RawConfig &config) override {
+    config_.load(config, true);
+    safeSaveAsIni(config_, "conf/m17n.conf");
+  }
 
-    std::vector<InputMethodEntry> listInputMethods() override;
+  std::vector<InputMethodEntry> listInputMethods() override;
 
 private:
-    Instance *instance_;
-    M17NConfig config_;
-    std::vector<OverrideItem> list_;
-    FactoryFor<M17NState> factory_;
+  Instance *instance_;
+  M17NConfig config_;
+  std::vector<OverrideItem> list_;
+  FactoryFor<M17NState> factory_;
 };
 
 class M17NEngineFactory : public AddonFactory {
-    fcitx::AddonInstance *create(fcitx::AddonManager *manager) override {
-        return new M17NEngine(manager->instance());
-    }
+  fcitx::AddonInstance *create(fcitx::AddonManager *manager) override {
+    return new M17NEngine(manager->instance());
+  }
 };
-}
+} // namespace fcitx
 
 #endif // _IM_ENGINE_H_
